522f2fa3e95566f523b6f52103409e65fa0a5307
CARDONALUIS <40583861+CARDONALUIS@users.noreply.github.com>
2021-06-27 21:57:30 -0500

    Problema de las ocho reinas ajedrez
    Problema de las ocho reinas ajedrez

---
+++ Reinas/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Reinas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.vScrollBar1);
+            this.panel1.Controls.Add(this.hScrollBar1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(838, 510);
+            this.panel1.TabIndex = 0;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.vScrollBar1.Location = new System.Drawing.Point(821, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 493);
+            this.vScrollBar1.TabIndex = 1;
+            this.vScrollBar1.ValueChanged += new System.EventHandler(this.VScrollBar1_ValueChanged);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hScrollBar1.Location = new System.Drawing.Point(0, 493);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(838, 17);
+            this.hScrollBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 510);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+    }
+}
+
---
+++ Reinas/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Reinas
+{
+    public partial class Form1 : Form
+    {
+        int r = 0;
+        int x = 100;
+        int y = 50;
+        string strButtonName = "btn_Tablero";  //Nombre que van a tener los botones del tablero
+
+        public Form1()
+        {
+            List<int> tab;
+            InitializeComponent();
+            
+            tab = creaTablero();
+            int contTab = 0;
+            //creaPaneles();
+            MessageBox.Show("Buscare la solucion, dame unos segundos, click en aceptar");
+            while(!esValido(tab))
+            {
+                tab = creaTablero();
+                //if(contTab < 10)
+                
+                r = 0;
+                contTab++;
+            }
+            creaPaneles(tab);
+            MessageBox.Show("!!!Encontre la solucion en la iteracion: " + contTab+"\nClick en aceptar para ver tablero\nSi las reinas no se ven, direccionar a la carpeta donde esta la imagen");
+            r = 0;
+        }
+
+        public bool esValido(List<int> tabPru)
+        {
+            int indCom = 0;
+
+            /*
+            //Solucion
+            tabPru.Clear();
+            tabPru.Add(1);
+            tabPru.Add(3);
+            tabPru.Add(5);
+            tabPru.Add(7);
+            tabPru.Add(2);
+            tabPru.Add(0);
+            tabPru.Add(6);
+            tabPru.Add(4);*/
+            
+            /*
+            //Falla 1
+            tabPru.Clear();
+            tabPru.Add(0);
+            tabPru.Add(3);
+            tabPru.Add(2);
+            tabPru.Add(1);
+            tabPru.Add(4);
+            tabPru.Add(7);
+            tabPru.Add(6);
+            tabPru.Add(5);
+            */
+
+            /*foreach(int a in tabPru.ElementAt(0))
+            {                
+                foreach (int b in tabPru.ElementAt(1))
+                {
+                    for(int i =0; i < 8; i++)
+                    {
+
+                    }
+                }               
+            }*/
+            bool band = true;
+
+
+            for (int i = 0; i < 8 && band; i++)
+            {
+                r = 0;
+                for(int j = 0; j<8 && band; j++)
+                {
+
+                    r = 0;
+                    if(i!=j)
+                    {
+                        r = 0;
+                        if (tabPru.ElementAt(i)+j == tabPru.ElementAt(j)+i || tabPru.ElementAt(i) - j == tabPru.ElementAt(j) - i)
+                        {
+                            r = 0;
+                            band = false;
+                            
+
+                        }
+                        //if (tabPru.ElementAt(0).ElementAt(i) != tabPru.ElementAt(0).ElementAt(j)+j && tabPru.ElementAt(1).ElementAt(i) != tabPru.ElementAt(1).ElementAt(i - j)) ;
+                    }
+                    
+                }
+            }
+
+            r = 0;
+
+
+            return band;
+        }
+
+        public List<int> creaTablero()
+        {
+            //List<List<int>> ListTabl = new List<List<int>>();
+
+            Random rnd = new Random();
+            List<int> lisCol = new List<int>();
+            //List<int> lisRen = new List<int>();
+
+            //RellenaTabl()
+            int numCol;
+            int numRen;
+
+            while(lisCol.Count!=8)
+            {
+                numCol = rnd.Next(8);
+
+                if(lisCol.Count == 0)
+                {
+                    lisCol.Add(numCol);
+                }
+                else
+                    if(!lisCol.Contains(numCol))
+                    {
+                    lisCol.Add(numCol);
+                    }
+
+            }
+            //ListTabl.Add(lisCol);
+
+            /*
+            while (lisRen.Count != 8)
+            {
+                numRen = rnd.Next(8);
+
+                if (lisRen.Count == 0)
+                {
+                    lisRen.Add(numRen);
+                }
+                else
+                    if (!lisRen.Contains(numRen))
+                {
+                    lisRen.Add(numRen);
+                }
+
+            }
+            ListTabl.Add(lisRen);*/
+
+            return (lisCol);
+        }
+
+        public void creaPaneles(List<int>tab)
+        {
+            int nBoton = 0; //Almaceno el numero de boton
+            for (int i = 0; i <  8; i++)
+            {
+                nBoton++; //Para dessincronizarlos, le sumo 1 al cambiar de fila
+                for (int j = 0; j < 8; j++)
+                {
+                    var btn = new Button();
+                    btn.Text = "";
+                    btn.Name = string.Format("{0}_{1}", strButtonName, nBoton); // Le doy un name que me permita identificar el boton
+                    btn.Tag = nBoton; // Le añado un tag, para permitirme ordenarlos
+                    btn.Location = new Point(x, y);
+                    btn.Size = new Size(50, 50);
+                    btn.BackColor = Convert.ToInt32(btn.Tag) % 2 == 0 ? Color.White : Color.Black; //Loas ares de un color, los impares de otro
+                                                                                                   //por 
+                    btn.BackColor = Convert.ToInt32(btn.Tag) % 2 == 0 ? Color.Black : Color.White; //Loas ares de un color, los impares de otro
+
+                    if (j == tab.ElementAt(i))//Aqui poner la ruta a la imagen, la imagen esta en la carpeta "bin\debug" del proyecto  
+                        btn.Image = Image.FromFile(Directory.GetCurrentDirectory() + "\\Reinas.png");
+                        
+                    panel1.Controls.Add(btn);
+                    x += 50;
+                    nBoton++;
+                }
+
+                y += 50;
+                x = 100;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            /*int nBoton = 0; //Almaceno el numero de boton
+            for (int i = 1; i <= 10; i++)
+            {
+                nBoton++; //Para dessincronizarlos, le sumo 1 al cambiar de fila
+                for (int j = 1; j <= 10; j++)
+                {
+                    var btn = new Button();
+                    btn.Text = "";
+                    btn.Name = string.Format("{0}_{1}", strButtonName, nBoton); // Le doy un name que me permita identificar el boton
+                    btn.Tag = nBoton; // Le añado un tag, para permitirme ordenarlos
+                    btn.Location = new Point(x, y);
+                    btn.Size = new Size(50, 50);
+
+
+                    this.Controls.Add(btn);
+                    x += 50;
+                    nBoton++;
+                }
+
+                y += 50;
+                x = 100;
+            }*/
+        }
+
+        private void VScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
